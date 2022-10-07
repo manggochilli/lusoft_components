d9111d0f4d55355f1659d076212f25f5f537e426
windcore <windcore@lusoft.co.kr>
2022-10-07 17:19:26 +0900

    로고 이미지 적용

--- src/pages/test_component.tsx
+++ src/pages/test_component.tsx
@@ -1,4 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import LogoBig from "../image/base/logo.png";
+import LogoSmall from "../image/base/logo_small.png";
+
+// components
 import { Button } from "../components/button";
 import { InputText } from "../components/inputText";
 import { Icon } from "../components/icon";
@@ -10,19 +16,10 @@ function TestComponent() {
 		<div className="pack w(100vw) p(20) bg(#000.5)">
 			<div className="relative w(360) h(850) bg(#fff) clip">
 
-				{/** <Image_Area/> */}
-				<div className="absolute top(84) left(57) pack w(246) h(128) r(5)">
-					<div className="w(246) h(55) b(1)">image</div>
-				</div>
-				{/** </Image_Area> */}
-
-
 				{/** <Top_navigation/> */}
 				<div className="absolute top(0) left(0) hbox hgap(222) p(10/0/0/8)">
-					<div className="vbox">
-						<div className="w(60) h(24) b(1)">image</div>
-					</div>
-					<div className="hbox hgap(9.199999809265137) p(0/8/0/5)">
+					<img src={LogoSmall} className="vbox w(60) h(24)" />
+					<div className="hbox hgap(9) p(0/8/0/5)">
 
 						{/** <Search/> */}
 						<Icon iconName="search" size={24} color="#404040" />
@@ -36,6 +33,9 @@ function TestComponent() {
 				</div>
 				{/** </Top_navigation> */}
 
+				{/** <Image_Area/> */}
+				<img src={LogoBig} className="absolute top(84) left(57) pack w(246) h(128) r(5)" />
+				{/** </Image_Area> */}
 
 				{/** <Text_input/> */}
 				<div className="absolute top(292) left(30) vbox w(300) bg(#fff)">
@@ -72,24 +72,24 @@ function TestComponent() {
 
 				<SNSbutton service="naver" size="large" iconPosition="left" usrText="네이버 ID로 로그인" usrclass="absolute top(600) left(5) w(350)" />
 
-				<SNSbutton service="kakao" size="large" iconPosition="text_left" usrText="카카오 ID로 로그인" usrclass="absolute top(660) left(5) w(350)" />
+				<SNSbutton service="kakao" size="large" iconPosition="left" usrText="카카오 ID로 로그인" usrclass="absolute top(660) left(5) w(350)" />
 
-				<SNSbutton service="google" size="large" iconPosition="center" usrText="Google ID로 로그인" usrclass="absolute top(720) left(5) w(350)" />
+				<SNSbutton service="google" size="large" iconPosition="left" usrText="Google ID로 로그인" usrclass="absolute top(720) left(5) w(350)" />
 
-				<SNSbutton service="apple" size="large" useFill="fill" iconPosition="right" usrText="Apple ID로 로그인" usrclass="absolute top(780) left(5) w(350)" />
+				<SNSbutton service="apple" size="large" useFill="fill" iconPosition="left" usrText="Apple ID로 로그인" usrclass="absolute top(780) left(5) w(350)" />
 
 				{/** <Login_group/> */}
 				<div className="absolute top(542) left(13) hbox">
 
-					<Button kind="ghost" usrclass="clip p(13/26) font(11/16) text-center">이메일 가입</Button>
+					<Link to="/" className="clip p(13/26) font(11/16) c(#262626) hover:c(#262626) text-center">이메일 가입</Link>
 
 					<div className="w(1) h(16) bg(#777)"></div>
 
-					<Button kind="ghost" usrclass="clip p(13/26) font(11/16) text-center">이메일 찾기</Button>
+					<Link to="/" className="clip p(13/26) font(11/16) c(#262626) hover:c(#262626) text-center">이메일 찾기</Link>
 
 					<div className="w(1) h(16) bg(#777)"></div>
 
-					<Button kind="ghost" usrclass="clip p(13/26) font(11/16) text-center">비밀번호 찾기</Button>
+					<Link to="/" className="clip p(13/26) font(11/16) c(#262626) hover:c(#262626) text-center">비밀번호 찾기</Link>
 
 				</div>
 				{/** </Login_group> */}
