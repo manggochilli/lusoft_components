1d5ea6ce2531ccdc4cbb4bdeebfdc5378be87c7d
windcore <windcore@lusoft.co.kr>
2022-10-07 17:51:36 +0900

    페이지 엘리먼트 정렬

--- src/pages/test_component.tsx
+++ src/pages/test_component.tsx
@@ -38,15 +38,15 @@ function TestComponent() {
 				{/** </Image_Area> */}
 
 				{/** <Text_input/> */}
-				<div className="absolute top(292) left(30) vbox w(300) bg(#fff)">
+				<div className="absolute top(250) left(30) vbox w(300) bg(#fff)">
 
 					{/** <_Text_input_base/> */}
 					<div className="vbox vgap(4)">
 						<div className="vbox vgap(8)">
-							<div className="w(300) font(12/18) c(#c9162b)">이메일 주소</div>
-							<InputText type="text" kind="underline" usrclass="w(300) h(48) bg(#fff) bc(#c9162b) placeholder:w(236) placeholder:pl(15) placeholder:font(14/20) placeholder:c(#a8a8a8)" placeholder="예) kream@kream.co.kr" />
+							<div className="w(300) font(12/18) c(#525252)">이메일 주소</div>
+							<InputText type="text" kind="underline" usrclass="w(300) h(48) p(0/40/0/10) bg(#fff) bc(#ccc) placeholder:w(236) placeholder:font(14/20) placeholder:c(#a8a8a8)" placeholder="예) kream@kream.co.kr" />
 						</div>
-						<div className="w(300) font(12/18) c(#c9162b)">이메일 주소를 정확히 입력해주세요.</div>
+						<div className="none w(300) font(12/18) c(#c9162b)">이메일 주소를 정확히 입력해주세요.</div>
 					</div>
 					{/** </_Text_input_base> */}
 
@@ -54,13 +54,13 @@ function TestComponent() {
 				{/** </Text_input> */}
 
 				{/** <Text_input/> */}
-				<div className="absolute top(400) left(30) vbox w(300) bg(#fff)">
+				<div className="absolute top(360) left(30) vbox w(300) bg(#fff)">
 
 					{/** <_Text_input_base/> */}
 					<div className="vbox">
 						<div className="vbox vgap(8)">
 							<div className="w(300) font(12/18) c(#525252)">비밀번호</div>
-							<InputText type="password" kind="underline" usrclass="w(300) h(48) bg(#fff) bc(#ccc) placeholder:w(236) placeholder:pl(15) placeholder:font(14/20) placeholder:c(#a8a8a8)" placeholder="비밀번호" />
+							<InputText type="password" kind="underline" usrclass="w(300) h(48) p(0/40/0/10) bg(#fff) bc(#ccc) placeholder:w(236) placeholder:font(14/20) placeholder:c(#a8a8a8)" placeholder="비밀번호" />
 						</div>
 					</div>
 					{/** </_Text_input_base> */}
@@ -68,18 +68,10 @@ function TestComponent() {
 				</div>
 				{/** </Text_input> */}
 
-				<Button kind="secondary" size="small" usrclass="absolute top(492) left(109) w(145) bg(#e8e8e8) font(14/20) c(#949494)">로그인</Button>
-
-				<SNSbutton service="naver" size="large" iconPosition="left" usrText="네이버 ID로 로그인" usrclass="absolute top(600) left(5) w(350)" />
-
-				<SNSbutton service="kakao" size="large" iconPosition="left" usrText="카카오 ID로 로그인" usrclass="absolute top(660) left(5) w(350)" />
-
-				<SNSbutton service="google" size="large" iconPosition="left" usrText="Google ID로 로그인" usrclass="absolute top(720) left(5) w(350)" />
-
-				<SNSbutton service="apple" size="large" useFill="fill" iconPosition="left" usrText="Apple ID로 로그인" usrclass="absolute top(780) left(5) w(350)" />
+				<Button kind="secondary" size="small" usrclass="absolute top(490) left(109) w(145) bg(#e8e8e8) font(14/20) c(#949494)">로그인</Button>
 
 				{/** <Login_group/> */}
-				<div className="absolute top(542) left(13) hbox">
+				<div className="absolute top(540) left(13) hbox">
 
 					<Link to="/" className="clip p(13/26) font(11/16) c(#262626) hover:c(#262626) text-center">이메일 가입</Link>
 
@@ -93,6 +85,14 @@ function TestComponent() {
 
 				</div>
 				{/** </Login_group> */}
+
+				<SNSbutton service="naver" size="medium" iconPosition="left" usrText="네이버 ID로 로그인" usrclass="absolute top(620) left(5) w(350)" />
+
+				<SNSbutton service="kakao" size="medium" iconPosition="left" usrText="카카오 ID로 로그인" usrclass="absolute top(670) left(5) w(350)" />
+
+				<SNSbutton service="google" size="medium" iconPosition="left" usrText="Google ID로 로그인" usrclass="absolute top(720) left(5) w(350)" />
+
+				<SNSbutton service="apple" size="medium" useFill="fill" iconPosition="left" usrText="Apple ID로 로그인" usrclass="absolute top(770) left(5) w(350)" />
 
 			</div>
 		</div>
