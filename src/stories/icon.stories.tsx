40bd202416f21fdd7e2d2e235bf907fba2b7b365
windcore <windcore@lusoft.co.kr>
2022-10-11 09:05:30 +0900

    아이콘 추가

---
+++ src/stories/icon.stories.tsx
@@ -0,0 +1,28 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Icon } from '../components/icon';
+
+export default {
+	title: 'LUsoft Components/Components/icon',
+	component: Icon,
+	argTypes: {
+		iconName: { control: 'select' },
+		size: { control: '' },
+		color: { control: '' },
+		usrclass: { control: '' },
+	},
+} as ComponentMeta<typeof Icon>;
+
+const Template: ComponentStory<typeof Icon> = (args) => <Icon {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+	iconName: 'menu',
+	size: 30,
+	color: '#444',
+	usrclass: 'b(1) bc(#ccc)',
+};
+
+Default.story = {
+	name: 'Default'
+}
