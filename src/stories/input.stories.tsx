295d36993b1e5aeba3d210fa81cee2df05e240ed
windcore <windcore@lusoft.co.kr>
2022-10-11 16:27:28 +0900

    inputText type 변경사항 적용

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -17,7 +17,6 @@ const Template: ComponentStory<typeof InputText> = (args) => <InputText {...args
 
 export const Default = Template.bind({});
 Default.args = {
-	type: 'text',
 	kind: 'box',
 	usrclass: '',
 	placeholder: '내용을 입력하세요',
@@ -30,7 +29,6 @@ Default.story = {
 // box
 export const Box = Template.bind({});
 Box.args = {
-	type: 'text',
 	kind: 'box',
 	usrclass: '',
 	placeholder: '내용을 입력하세요',
@@ -43,7 +41,6 @@ Box.story = {
 // underline
 export const Underline = Template.bind({});
 Underline.args = {
-	type: 'text',
 	kind: 'underline',
 	usrclass: '',
 	placeholder: '내용을 입력하세요',
@@ -56,7 +53,6 @@ Underline.story = {
 // ghost
 export const Ghost = Template.bind({});
 Ghost.args = {
-	type: 'text',
 	kind: 'ghost',
 	usrclass: '',
 	placeholder: '내용을 입력하세요',
