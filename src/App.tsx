a081803ed48fb378300aedf544e32db55ff92ab1
windcore <windcore@lusoft.co.kr>
2022-10-17 11:38:46 +0900

    페이지 이동 메뉴 연결

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import "./css/theme.css";
 
 // Pages
 import NotFound from './pages/_404';
+import Header from './pages/header';
 import Index from './pages/index';
 import PageTestHandShake from './pages/test_handshake';
 import PageTestComponent from './pages/test_component';
@@ -12,6 +13,7 @@ function App() {
 	return (
 		<div className="relative w(100vw) sm h(100vh) overflow-x(hidden)">
 			<BrowserRouter>
+				<Header />
 				<Routes>
 					<Route index element={<Index />} />
 					<Route path="/test/handshake" element={<PageTestComponent />} />
