3c58e1967b49c02e96f984aed9c5f0dca767d6b3
windcore <windcore@lusoft.co.kr>
2022-10-17 16:40:50 +0900

    swiper 페이지 router

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,12 @@ import ScrollToTop from './pages/_scroll_top';
 
 // Pages
 import NotFound from './pages/_404';
-import Header from './pages/header';
 import Index from './pages/index';
+
+// Test Pages
 import PageTestHandShake from './pages/test_handshake';
 import PageTestComponent from './pages/test_component';
+import PageTestSwiperJS from './pages/test_swiper';
 
 function App() {
 	return (
@@ -24,6 +26,7 @@ function App() {
 				</Route>
 				<Route path="/test/handshake" element={<DefaultLayout><PageTestHandShake /></DefaultLayout>} />
 				<Route path="/test/component" element={<DefaultLayout><PageTestComponent /></DefaultLayout>} />
+				<Route path="/test/swiper" element={<DefaultLayout><PageTestSwiperJS /></DefaultLayout>} />
 				<Route path="/*" element={<NotFound />} />
 			</Routes>
 		</BrowserRouter>
