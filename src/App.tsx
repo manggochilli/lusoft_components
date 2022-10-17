5e6759af7b7a1c2b7f978af5d0469a62eb6707e8
windcore <windcore@lusoft.co.kr>
2022-10-17 14:31:36 +0900

    scrollTop 처리

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./css/theme.css";
 
+// scrolltop
+import ScrollToTop from './pages/_scroll_top';
+
 // Pages
 import NotFound from './pages/_404';
 import Header from './pages/header';
@@ -11,17 +14,16 @@ import PageTestComponent from './pages/test_component';
 
 function App() {
 	return (
-		<div className="relative w(100vw) sm h(100vh) overflow-x(hidden)">
-			<BrowserRouter>
-				<Header />
-				<Routes>
-					<Route index element={<Index />} />
-					<Route path="/test/handshake" element={<PageTestHandShake />} />
-					<Route path="/test/component" element={<PageTestComponent />} />
-					<Route path="/*" element={<NotFound />} />
-				</Routes>
-			</BrowserRouter>
-		</div>
+		<BrowserRouter>
+			<ScrollToTop />
+			<Routes>
+				<Route index element={<Index />} />
+				<Route path="/test/handshake" element={<PageTestHandShake />} />
+				<Route path="/test/component" element={<PageTestComponent />} />
+				<Route path="/*" element={<NotFound />} />
+			</Routes>
+			<Header />
+		</BrowserRouter>
 	)
 }
 
