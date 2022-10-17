7eac60327b75588f8c8ff16fc8e5ab2b56fbffbe
windcore <windcore@lusoft.co.kr>
2022-10-17 16:26:25 +0900

    footer style 변경

--- src/layout/footer/index.tsx
+++ src/layout/footer/index.tsx
@@ -1,6 +1,6 @@
 export default function DefaultFooter() {
 	return (
-		<footer className="hpack grid gap(10px) w(100vw) c(#ccc)">
+		<footer className="hpack grid gap(10px) w(100vw) bg(#303030) font(12) c(#505050)">
 			components test page
 		</footer>
 	);
