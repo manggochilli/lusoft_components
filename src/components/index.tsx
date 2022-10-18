e9611e000fedc41eacd0f93627094579782e3f9a
windcore <windcore@lusoft.co.kr>
2022-10-18 13:53:35 +0900

    컴포넌트 모음에 탭 추가

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,5 +3,6 @@ import { Icon } from './icon';
 import { SNSbutton } from './snsbutton';
 import { InputText } from './inputText';
 import { InputPass } from './inputPass';
+import { Tabs } from './tabs';
 
-export { Button, Icon, SNSbutton, InputText, InputPass };
+export { Button, Icon, SNSbutton, InputText, InputPass, Tabs };
