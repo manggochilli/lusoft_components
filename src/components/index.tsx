78aeaffca6cc4ae4ee517763a6871abc49f8720b
windcore <windcore@lusoft.co.kr>
2022-10-18 15:50:51 +0900

    tab 컴포넌트 삭제

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,6 +3,5 @@ import { Icon } from './icon';
 import { SNSbutton } from './snsbutton';
 import { InputText } from './inputText';
 import { InputPass } from './inputPass';
-import { Tabs } from './tabs';
 
-export { Button, Icon, SNSbutton, InputText, InputPass, Tabs };
+export { Button, Icon, SNSbutton, InputText, InputPass };
