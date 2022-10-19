df5d3e0fa812c22016b0ee52330c97a0cba83638
windcore <windcore@lusoft.co.kr>
2022-10-19 15:30:44 +0900

    Text 추가

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -4,5 +4,6 @@ import { SNSbutton } from './snsbutton';
 import { InputText } from './inputText';
 import { InputPass } from './inputPass';
 import { Stick } from './stick';
+import { Text } from './text';
 
-export { Button, Icon, SNSbutton, InputText, InputPass, Stick };
+export { Button, Icon, SNSbutton, InputText, InputPass, Stick, Text };
