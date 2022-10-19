e5df09c73b93955caf32e1bc8d38bcbc136f5ff5
windcore <windcore@lusoft.co.kr>
2022-10-19 16:53:49 +0900

    size 옵션 추가

--- src/components/Stick/index.tsx
+++ src/components/Stick/index.tsx
@@ -1,7 +1,27 @@
-export const Stick = () => {
+interface StickProps {
+	/**
+	 * 크기 설정
+	 */
+	size?: '' | 'small' | 'medium' | 'large' | 'expressive' | 'extralarge';
+}
+
+export const Stick = ({
+	size = 'large',
+	...props
+}: StickProps) => {
+	const sizeclass = size === 'small' ?
+		`w(1) h(16) m(0/5) bg(--default-border-color) `
+	: size === 'medium' ?
+		`w(1) h(19) m(0/5) bg(--default-border-color) `
+	: size === 'expressive' ?
+		`w(1) h(24) m(0/5) bg(--default-border-color) `
+	: size === 'extralarge' ?
+		`w(1) h(33) m(0/5) bg(--default-border-color) `
+	: // Default large
+		`w(1) h(24) m(0/5) bg(--default-border-color) `;
 	return (
 		<div
-			className={`w(1) h(15) m(0/5) bg(--default-border-color)`}
+			className={[sizeclass].join(' ')}
 		/>
 	);
 };
