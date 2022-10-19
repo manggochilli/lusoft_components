76d4d57a0a77244d20d2def2446c16a298fc04e5
windcore <windcore@lusoft.co.kr>
2022-10-19 14:57:10 +0900

    옵션 제거

--- src/components/Stick/index.tsx
+++ src/components/Stick/index.tsx
@@ -1,41 +1,7 @@
-import React from 'react';
-
-interface StickProps {
-	/**
-	 * 높이
-	 */
-	height?: number,
-	/**
-	 * 두께
-	 */
-	weight?: number,
-	/**
-	 * 색상
-	 */
-	color?: string,
-	/**
-	 * 사용자 추가 클래스
-	 */
-	usrclass?: string;
-}
-
-/**
- *  버튼 엘리먼트 정의
- */
-export const Stick = ({
-	height,
-	weight,
-	color,
-	usrclass,
-	...props
-}: StickProps) => {
-	const height_class = height && `h(${height}px)`;
-	const weight_class = weight && `w(${weight}px)`;
-	const color_class = color && `bg(${color})`;
+export const Stick = () => {
 	return (
 		<div
-			className={[height_class, weight_class, color_class, usrclass].join(' ')}
-			{...props}
+			className={`w(1) h(15) m(0/5) bg(--default-border-color)`}
 		/>
 	);
 };
