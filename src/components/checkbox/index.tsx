f673b5ba83ea38eaecf6485a40ffc4d640af525c
windcore <windcore@lusoft.co.kr>
2022-10-20 15:09:45 +0900

    checkbox 컴포넌트 옵션 처리

--- src/components/checkbox/index.tsx
+++ src/components/checkbox/index.tsx
@@ -1,9 +1,41 @@
-import { useState } from 'react';
-
-function Checkbox() {
+interface CheckboxProps {
+	/**
+	 * 셋업된 분류
+	 */
+	kind?: 'normal' | 'primary_fill' | 'primary_ghost';
+	/**
+	 * 크기 설정
+	 */
+	size?: '' | 'small' | 'medium' | 'large' | 'expressive' | 'extralarge';
+	/**
+	 * 이름
+	 */
+	_name?: string;
+}
+export const Checkbox = ({
+	kind,
+	size,
+	_name,
+	...props
+}: CheckboxProps) => {
+	const baseClass = `pointer before:block+b(1)+bc(--default-border-color)+content('') &:checked:before:b(1)+content('✔') &:checked:before:text-center`;
+	const kindClass = kind === 'primary_fill' ?
+		`&:checked:before:bc(--primary-border-color)+bg(--primary-bg-color) &:checked:before:c(--primary-fill-font-color)`
+	: kind === 'primary_ghost' ?
+		`&:checked:before:bc(--primary-border-color)+bg(--white-color) &:checked:before:c(--primary-font-color)`
+	: // default normal
+		`&:checked:before:bc(--default-border-color)+bg(--white-color) &:checked:before:c(--default-font-color)`;
+	const sizeclass = size === 'small' ?
+		`before:w(14)+h(14) &:checked:before:w(14)+h(14)+font(12) &:checked:before:line-height(12)`
+	: size === 'large' ?
+		`before:w(22)+h(22) &:checked:before:w(22)+h(22)+font(20) &:checked:before:line-height(20)`
+	: size === 'expressive' ?
+		`before:w(24)+h(24) &:checked:before:w(24)+h(24)+font(24) &:checked:before:line-height(24)`
+	: size === 'extralarge' ?
+		`before:w(31)+h(31) &:checked:before:w(31)+h(31)+font(29) &:checked:before:line-height(29)`
+	:	// default medium
+		`before:w(18)+h(18) &:checked:before:w(18)+h(18)+font(16) &:checked:before:line-height(16)`;
 	return (
-		<div></div>
+		<input type="checkbox" id={_name} name={_name} className={[baseClass,kindClass,sizeclass].join(' ')} />
 	);
 }
-
-export default Checkbox;
