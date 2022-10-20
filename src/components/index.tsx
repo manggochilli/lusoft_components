3c19e5a870446ba4f7774b5d37467d00fce57dce
windcore <windcore@lusoft.co.kr>
2022-10-20 14:30:00 +0900

    checkbox 추가

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -5,5 +5,6 @@ import { InputText } from './inputText';
 import { InputPass } from './inputPass';
 import { Stick } from './stick';
 import { Text } from './text';
+import { Checkbox } from './checkbox';
 
-export { Button, Icon, SNSbutton, InputText, InputPass, Stick, Text };
+export { Button, Icon, SNSbutton, InputText, InputPass, Stick, Text, Checkbox };
